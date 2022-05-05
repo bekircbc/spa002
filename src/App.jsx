da4c2021bbc3f12f27c6f2131b15cf5f41087fc8
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-05 13:52:31 +0200

    9 person added to data and style added

--- src/App.jsx
+++ src/App.jsx
@@ -9,41 +9,47 @@ function App() {
   return (
     <div className="App">
       <h1>HR Abteilung</h1>
-      {filterAbteilung("HR").map((m, index) => {
-        return (
-          <div key="index" className="person">
-            <img src={m.picturesrc} alt={m.lastName} />
-            <div className="name">
-              {m.firstName} {m.lastName}{" "}
+      <div className="abteilung">
+        {filterAbteilung("HR").map((m, index) => {
+          return (
+            <div key="index" className="person">
+              <img src={m.picturesrc} alt={m.lastName} />
+              <div className="name">
+                {m.firstName} {m.lastName}{" "}
+              </div>
+              <div className="age">{m.age}</div>
             </div>
-            <div className="age">{m.age}</div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
       <h1>IT Abteilung</h1>
-      {filterAbteilung("IT").map((m, index) => {
-        return (
-          <div key="index" className="person">
-            <img src={m.picturesrc} alt={m.lastName} />
-            <div className="name">
-              {m.firstName} {m.lastName}{" "}
+      <div className="abteilung">
+        {filterAbteilung("IT").map((m, index) => {
+          return (
+            <div key="index" className="person">
+              <img src={m.picturesrc} alt={m.lastName} />
+              <div className="name">
+                {m.firstName} {m.lastName}{" "}
+              </div>
+              <div className="age">{m.age}</div>
             </div>
-            <div className="age">{m.age}</div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
       <h1>Einkauf Abteilung</h1>
-      {filterAbteilung("Einkauf").map((m, index) => {
-        return (
-          <div key="index" className="person">
-            <img src={m.picturesrc} alt={m.lastName} />
-            <div className="name">
-              {m.firstName} {m.lastName}{" "}
+      <div className="abteilung">
+        {filterAbteilung("Einkauf").map((m, index) => {
+          return (
+            <div key="index" className="person">
+              <img src={m.picturesrc} alt={m.lastName} />
+              <div className="name">
+                {m.firstName} {m.lastName}{" "}
+              </div>
+              <div className="age">{m.age}</div>
             </div>
-            <div className="age">{m.age}</div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }

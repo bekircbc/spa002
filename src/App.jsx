54baee775e64269d28c2c2c2cca580f88e4697b7
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-05 13:45:25 +0200

    abteilung for firma added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,51 @@
-import { useState } from "react";
 import "./App.css";
 import data from "./data/data.json";
 
-function App() {
-  const [count, setCount] = useState(0);
+const filterAbteilung = (abt = "HR") => {
+  return data.filter((m) => m.abteilung === abt);
+};
 
-  return <div className="App">Hello!</div>;
+function App() {
+  return (
+    <div className="App">
+      <h1>HR Abteilung</h1>
+      {filterAbteilung("HR").map((m, index) => {
+        return (
+          <div key="index" className="person">
+            <img src={m.picturesrc} alt={m.lastName} />
+            <div className="name">
+              {m.firstName} {m.lastName}{" "}
+            </div>
+            <div className="age">{m.age}</div>
+          </div>
+        );
+      })}
+      <h1>IT Abteilung</h1>
+      {filterAbteilung("IT").map((m, index) => {
+        return (
+          <div key="index" className="person">
+            <img src={m.picturesrc} alt={m.lastName} />
+            <div className="name">
+              {m.firstName} {m.lastName}{" "}
+            </div>
+            <div className="age">{m.age}</div>
+          </div>
+        );
+      })}
+      <h1>Einkauf Abteilung</h1>
+      {filterAbteilung("Einkauf").map((m, index) => {
+        return (
+          <div key="index" className="person">
+            <img src={m.picturesrc} alt={m.lastName} />
+            <div className="name">
+              {m.firstName} {m.lastName}{" "}
+            </div>
+            <div className="age">{m.age}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
